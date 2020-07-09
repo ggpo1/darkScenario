70016fe23558c8ca43db5500a6019402e2df08a5
ggpo1 <johnlevis99@gmail.com>
2020-07-10 02:04:00 +0300

    test

--- App.tsx
+++ App.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Brightness from 'expo-brightness';
+import { useKeepAwake } from 'expo-keep-awake';
+
+enum AppMode {
+  LIST,
+  RUN,
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      
-    </View>
-  );
+  const [mode, setMode] = useState<AppMode>(AppMode.RUN);
+  const [bright, setBright] = useState<number>(0);
+  useKeepAwake();
+  useEffect(() => {
+    (async () => {
+      const { status } = await Brightness.requestPermissionsAsync();
+      if (status === 'granted') {
+        Brightness.setSystemBrightnessAsync(0);
+      }
+    })();
+  }, []);
+
+  let content = (<View></View>);
+  if (mode === AppMode.LIST) {
+    Brightness.setBrightnessAsync(0.5);
+  } else if (mode == AppMode.RUN) {
+    content = (
+      <TouchableOpacity
+        activeOpacity={100}
+        onPress={() => { 
+          if (bright === 1) {
+            Brightness.setBrightnessAsync(0);
+            setBright(0);
+          } else if (bright === 0) {
+            Brightness.setBrightnessAsync(1);
+            setBright(1);
+          }
+        }}
+        style={styles.container}>
+      </TouchableOpacity>
+    );
+  }
+
+
+  return content;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
